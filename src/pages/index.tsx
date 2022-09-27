b474c0c5e5eb55f7b9f4ce7b98ba3ea008803440
secose <secose.website@gmail.com>
2022-09-27 16:11:17 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,123 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 550,
+}
+const headingAccentStyles = {
+  color: "#663399",
+}
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
+}
+const doclistStyles = {
+  paddingLeft: 0,
+}
+
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
+}
+
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+}
+
+const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: "none",
+  display: `inline-block`,
+  marginBottom: 24,
+  marginRight: 12,
+}
+
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
+}
+
+const docLinks = [
+  {
+    text: "twitter",
+    url: "https://twitter.com/SecoseCfd",
+    color: "#8954A8",
+  },
+
+]
+
+
+
+const badgeStyle = {
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
+  fontSize: 11,
+  fontWeight: "bold",
+  letterSpacing: 1,
+  borderRadius: 4,
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative" as "relative",
+  top: -2,
+  marginLeft: 10,
+  lineHeight: 1,
+}
+
+
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>
+        secose.
+        <br />
+        <span style={headingAccentStyles}>— crowdfunding that you need</span>
+      </h1>
+      <p style={paragraphStyles}>
+        For business inquiries, please contact us at <code style={codeStyles}><a style={{ textDecoration: "none", color: "#8A6534" }} href="mailto:secose.cfd@gmail.com">secose.cfd@gmail.com</a></code>
+      </p>
+      <ul style={doclistStyles}>
+        {docLinks.map(doc => (
+          <li key={doc.url} style={docLinkStyle}>
+            <a
+              style={linkStyle}
+              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
+            >
+              {doc.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </main >
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
